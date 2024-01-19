b8d2eea4b4868fc80fae88b7e07e2fed04548f2d
JI-Maina <irungujmaina@gmail.com>
2024-01-19 12:05:28 +0300

    introduced pages for different users

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,36 +3,27 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/options";
 import NavigationCard from "@/components/home/navigation-card";
 
-// interface Session {
-//   user: {
-//     user_role: string;
-//     // other properties...
-//   };
-//   access_token: string;
-//   exp: number;
-//   refresh_token: string;
-//   // other properties...
-// }
-type User = {
-  user_role: string;
-};
-
 const HomePage = async () => {
   const session = await getServerSession(authOptions);
-  console.log(session?.user);
+  // console.log(session?.user);
+
   if (session && session.user) {
-    const { user } = session;
-    if (user.user_role === "is_competition_owner") {
+    // destructure user_role
+    const {
+      user: { user_role },
+    } = session;
+
+    if (user_role === "is_competition_owner") {
       return <NavigationCard url={"/competition"} />;
-    } else if (session.user.user_role === "is_team_staff") {
+    } else if (user_role === "is_team_staff") {
       return <NavigationCard url={"/team"} />;
-    } else if (session.user.user_role === "is_tisini_staff") {
+    } else if (user_role === "is_tisini_staff") {
       return <NavigationCard url={"/tisini"} />;
-    } else if (session.user.user_role === "is_player") {
+    } else if (user_role === "is_player") {
       return <NavigationCard url={"/player"} />;
-    } else if (session.user.user_role === "is_referee") {
+    } else if (user_role === "is_referee") {
       return <NavigationCard url={"/referee"} />;
-    } else if (session.user.user_role === "is_agent") {
+    } else if (user_role === "is_agent") {
       return <NavigationCard url={"/agent"} />;
     }
   }
