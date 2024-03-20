5f440aaa0966eeeab349cabd4767a3b3afaf9de8
McMaina <irutujmaina@gmail.com>
2024-03-20 18:04:57 +0300

    modified players page

--- src/app/home/team/components/players/team-players.tsx
+++ src/app/home/team/components/players/team-players.tsx
@@ -2,32 +2,52 @@
 
 import { useEffect, useState } from "react";
 
-import TeamsHeader from "../teams-header";
 import { columns } from "./columns";
 import { PlayersTable } from "./players-table";
+import TeamsHeader from "../shared/teams-header";
+import HeaderButton from "../header-button";
 
 const TeamPlayers = ({ teams }: { teams: Team[] }) => {
   const [id, setId] = useState(teams?.[0]?.id);
+  const [teamId, setTeamId] = useState("");
   const [players, setPlayers] = useState([]);
 
   const team = teams.find((team) => team.id === id);
 
+  const onChange = (value: string) => {
+    setId(value);
+    setTeamId("");
+  };
+
+  const onTeamChange = (value: string) => {
+    setTeamId(value);
+  };
+
   useEffect(() => {
+    const parentId = teamId != "" ? teamId : id;
+
     const getPlayers = async () => {
-      const res = await fetch(`/api/players/${team?.id}`);
+      const res = await fetch(`/api/players/${parentId}`);
       const data = await res.json();
       setPlayers(data);
     };
 
-    if (team) getPlayers();
-  }, [team]);
-
+    if (parentId) getPlayers();
+  }, [id, teamId]);
+  console.log(players);
   return (
     <main className="space-y-2">
       <TeamsHeader
-        title="player"
-        team={team as Team}
-        url={`/home/team/teams/players/${team?.id}/add-new`}
+        teams={teams}
+        onChange={onChange}
+        id={id}
+        onTeamChange={onTeamChange}
+        modal={
+          <HeaderButton
+            title={"player"}
+            url={`/home/team/teams/players/${team?.id}/add-new`}
+          />
+        }
       />
 
       <section className="flex-grow">
