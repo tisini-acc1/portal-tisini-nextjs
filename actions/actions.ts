ba0ff0defd9437d4f5e904773df43eddc1f52fa7
McMaina <irutujmaina@gmail.com>
2024-11-08 09:50:54 +0300

    update cookies

--- actions/actions.ts
+++ actions/actions.ts
@@ -7,21 +7,23 @@ export async function handleLogin(
   accessToken: string,
   role: string
 ) {
-  cookies().set("session_userId", userId, {
+  const cookieStore = await cookies();
+
+  cookieStore.set("session_userId", userId, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     maxAge: 60 * 60 * 24 * 7, // One week
     path: "/",
   });
 
-  cookies().set("session_access_token", accessToken, {
+  cookieStore.set("session_access_token", accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     maxAge: 60 * 60 * 24 * 7, // One week
     path: "/",
   });
 
-  cookies().set("session_role", role, {
+  cookieStore.set("session_role", role, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     maxAge: 60 * 60 * 24 * 7, // One week
@@ -30,17 +32,23 @@ export async function handleLogin(
 }
 
 export async function resetAuthCookies() {
-  cookies().set("session_userId", "");
-  cookies().set("session_access_token", "");
-  cookies().set("session_role", "");
+  const cookieStore = await cookies();
+
+  cookieStore.set("session_userId", "");
+  cookieStore.set("session_access_token", "");
+  cookieStore.set("session_role", "");
 }
 
 export async function getUserId() {
-  const userId = cookies().get("session_userID")?.value;
+  const cookieStore = await cookies();
+
+  const userId = cookieStore.get("session_userID")?.value;
   return userId ? userId : null;
 }
 
 export async function getUserRole() {
-  const role = cookies().get("session_role")?.value;
+  const cookieStore = await cookies();
+
+  const role = cookieStore.get("session_role")?.value;
   return role ? role : null;
 }
