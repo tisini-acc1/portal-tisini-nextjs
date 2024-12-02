c41f168262f73bfab3e4b416d5b148ebd3ad0705
McMaina <irutujmaina@gmail.com>
2024-12-02 08:32:48 +0300

    test route

--- app/home/competitions/players/page.tsx
+++ app/home/competitions/players/page.tsx
@@ -1,7 +1,33 @@
+import { getToken } from "@/actions/actions";
+import axios from "axios";
 import React from "react";
 
-const PlayersPage = () => {
-  return <div>PlayersPage</div>;
+const PlayersPage = async () => {
+  const token = await getToken();
+
+  const getTournaments = async () => {
+    try {
+      const res = await axios.get(
+        `https://backend.tisini.co.ke/api/tournaments/${token}/`
+      );
+
+      return res.data;
+      // console.log("server", res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const data: Promise<CompTeam[]> = getTournaments();
+  const tournaments = await data;
+
+  return (
+    <>
+      {tournaments.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </>
+  );
 };
 
 export default PlayersPage;
