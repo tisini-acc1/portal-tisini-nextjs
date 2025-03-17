e70fa26e6fd8fe9142843b4884fc7300d41206a7
McMaina <irutujmaina@gmail.com>
2025-03-17 20:40:10 +0300

    added logic to upload cert url

--- components/profile/profile-image.tsx
+++ components/profile/profile-image.tsx
@@ -5,9 +5,17 @@ import Image from "next/image";
 
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { uploadImage } from "@/actions/upload";
+import { useStore } from "@/lib/store";
+import { useMutation } from "@tanstack/react-query";
+import { uploadPhotoUrl } from "@/actions/php-actions";
 
 const ProfileImage = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const { toast } = useToast();
+  const { store } = useStore((state) => state);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -18,8 +26,61 @@ const ProfileImage = () => {
     }
   };
 
-  const handleSubmit = () => {
-    console.log("first");
+  const mutation = useMutation({
+    mutationFn: uploadPhotoUrl,
+    onSuccess: (data) => {
+      console.log(data);
+      if (data.error === "0") {
+        // setOpen(false);
+        // setIsUploading(false);
+        setSelectedFile(null);
+        // queryClient.invalidateQueries({ queryKey: ["userCertifications"] });
+        toast({ title: "Success", description: data.message });
+      } else {
+        // setIsUploading(false);
+        toast({
+          title: "Error!",
+          variant: "destructive",
+          description: data.message,
+        });
+      }
+    },
+    onError: (error) => {
+      console.log(error);
+      toast({
+        title: "Error!",
+        variant: "destructive",
+        description: "An error occured while creating player",
+      });
+    },
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!selectedFile) {
+      toast({
+        title: "No File Selected",
+        description: "Please select a file before submitting.",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    const formData = new FormData(e.target as HTMLFormElement);
+    formData.append("userId", store.user.id);
+
+    const res = await uploadImage(formData);
+
+    if (res.error === "0") {
+      mutation.mutate({ url: res.url!, id: store.user.id });
+    } else {
+      toast({
+        title: "Error!",
+        variant: "destructive",
+        description: res.message,
+      });
+    }
   };
 
   return (
