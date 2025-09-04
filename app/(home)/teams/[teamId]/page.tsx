cfa294256a2355c4ae716488c0cacacfb1b99b7e
mcmaina <irutujmaina@gmail.com>
2025-09-04 19:00:20 +0300

    changed teeams homepage to a client component

--- app/(home)/teams/[teamId]/page.tsx
+++ app/(home)/teams/[teamId]/page.tsx
@@ -1,10 +1,25 @@
+"use client";
+
 import { getTeamOverview } from "@/data/overview/overview";
 import TeamOverview from "@/app/(home)/teams/components/teams/overview/team-overview";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../../loading";
 
-const TeamsPage = async () => {
-  const data = await getTeamOverview();
+const TeamsPage = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["overview"],
+    queryFn: () => getTeamOverview(),
+  });
 
-  return <TeamOverview overviewData={data} />;
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <div>Error...</div>;
+  }
+
+  return <TeamOverview overviewData={data as TeamOverview[]} />;
 };
 
 export default TeamsPage;
--- data/overview/overview.ts
+++ data/overview/overview.ts
@@ -1,19 +1,17 @@
-import "server-only";
-import { getToken } from "@/actions/actions";
+"use server";
+
 import { cache } from "react";
+import { getToken } from "@/actions/actions";
 
 export const getTeamOverview = cache(async (): Promise<TeamOverview[]> => {
   const token = await getToken();
   const baseURL = process.env.NEXT_PUBLIC_DJANGO_BASE_URL;
-
-  console.log(`${baseURL}/api/team_overview/${token}/`);
 
   try {
     const res = await fetch(`${baseURL}/api/team_overview/${token}/`, {
       next: { revalidate: 60, tags: ["team-overview"] },
     });
 
-    //   console.log("server 137:", res.data);
     if (!res.ok)
       throw new Error(`Failed to fetch team overview: ${res.status}`);
 
