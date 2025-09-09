0510389ef1386b88f188787a9e222d9d6424c8c3
mcmaina <irutujmaina@gmail.com>
2025-09-09 23:24:34 +0300

    removed dynamic routes

--- app/(dashboard)/teams/page.tsx
+++ app/(dashboard)/teams/page.tsx
@@ -1,11 +1,31 @@
-import { getUserTeams } from "@/data/teams/user-teams";
+"use client";
 
-import TeamsRouter from "./teams-router";
+import { useQuery } from "@tanstack/react-query";
+// import { getTeamOverview } from "@/data/overview/overview";
+import Loading from "../loading";
+import { getTeamOverview } from "@/data/overview/overview";
+import TeamOverview from "@/components/teams/overview/team-overview";
 
-const TeamsPage = async () => {
-  const data = await getUserTeams();
+const TeamsPage = () => {
+  // const data = await getTeamOverview();
 
-  return <TeamsRouter teams={data} />;
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["overview"],
+    queryFn: () => getTeamOverview(),
+  });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    console.log(isError);
+    return <div>Error</div>;
+  }
+
+  console.log(data);
+
+  return <TeamOverview overviewData={data as TeamOverview[]} />;
 };
 
 export default TeamsPage;
